af7593cc602fbf7859b85ba27e4a70ce586a5437
Brian Addicks <brian@addicks.us>
2019-06-27 07:24:51 -0400

    added Get-PsInventory

---
+++ PowerSwitch/Public/Generic/Get-PsInventory.ps1
@@ -0,0 +1,40 @@
+function Get-PsInventory {
+    [CmdletBinding(DefaultParametersetName = "path")]
+
+    Param (
+        [Parameter(Mandatory = $True, Position = 0, ParameterSetName = 'path')]
+        [string]$ConfigPath,
+
+        [Parameter(Mandatory = $True, Position = 0, ParameterSetName = 'array')]
+        [array]$ConfigArray,
+
+        [Parameter(Mandatory = $True, Position = 1)]
+        [ValidateSet('ExtremeEos')]
+        [string]$PsSwitchType
+    )
+
+    # It's nice to be able to see what cmdlet is throwing output isn't it?
+    $VerbosePrefix = "Get-PsInventory:"
+
+    # Check for path and import
+    if ($ConfigPath) {
+        if (Test-Path $ConfigPath) {
+            $LoopArray = Get-Content $ConfigPath
+        }
+    } else {
+        $LoopArray = $ConfigArray
+    }
+
+    # Get the switch type
+    switch ($PsSwitchType) {
+        'ExtremeEos' {
+            $ReturnObject = Get-EosInventory -ConfigArray $LoopArray
+        }
+        default {
+            Throw "$VerbosePrefix SwitchType not handled '$PsSwitchType'"
+        }
+    }
+
+
+    return $ReturnObject
+}
